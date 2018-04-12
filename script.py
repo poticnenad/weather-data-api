a9541279b55a8fd52b47d500181a9df19da2d637
Nenad Potic <nenad@debian>
2018-04-12 18:31:26 +0200

    Initial upload

---
+++ script.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    city = request.form['city']
+    if not len(city) > 0:
+        city = "Niš"
+    url = "http://api.apixu.com/v1/current.json?key=66ed38ae0ae842ac9fb124950181204&q=" + city
+    r = requests.get(url)
+    json_obj = r.json()
+    country = str(json_obj['location']['country'])
+    region = str(json_obj['location']['region'])
+    localtime = str(json_obj['location']['localtime'])
+    last_updated = str(json_obj['current']['last_updated'])
+    temp = str(json_obj['current']['temp_c'])
+    pa = str(json_obj['current']['pressure_mb'])
+    wind_kph = str(json_obj['current']['wind_kph'])
+    wind_dir = str(json_obj['current']['wind_dir'])
+    wind_deg = str(json_obj['current']['wind_degree'])
+    hum = str(json_obj['current']['humidity'])
+    vis_km = str(json_obj['current']['vis_km'])
+    precip_mm = str(json_obj['current']['precip_mm'])
+    icon = str(json_obj['current']['condition']['icon'])
+    return render_template('result.html',
+                           city=city, country=country, region=region, localtime=localtime,
+                           temp=temp, pa=pa, vis_km=vis_km,
+                           wind_kph=wind_kph, wind_dir=wind_dir, wind_deg=wind_deg,
+                           hum=hum, precip_mm=precip_mm, last_updated=last_updated, icon=icon)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
